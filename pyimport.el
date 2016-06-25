b697ff84130b6a60a5e0eee1f441ed6aab774259
Wilfred Hughes <me@wilfred.me.uk>
2016-06-25 22:47:23 +0100

    Add TODOs

--- pyimport.el
+++ pyimport.el
@@ -199,6 +199,8 @@ on line number LINE, remove VAR (e.g. 'baz')."
                   (looking-at (rx "import " (1+ (not (any space))) " as " line-end)))
           (pyimport--delete-current-line)))))))
 
+;; TODO: defcustom
+;; TODO: use executable-find
 (defvar pyimport-pyflakes-path nil
   "Path to pyflakes executable.
 Required for `pyimport-remove-unused'.")
