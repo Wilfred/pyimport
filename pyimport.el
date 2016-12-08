5435640f4ec402b5df8874aa3dd618736e5b273e
Wilfred Hughes <whughes@ahl.com>
2016-12-08 15:51:12 +0000

    Name the symbol that we couldn't find
    If the user doesn't have point on the correct variable, this helps them
    realise what's wrong.

--- pyimport.el
+++ pyimport.el
@@ -141,7 +141,7 @@ Dumb: just scans open Python buffers."
                (line (pyimport--import-simplify example-line symbol)))
           (pyimport--insert-import line)
           (message "%s (from %s)" line (get-text-property 0 'pyimport-path example-line)))
-      (user-error "No matches found"))))
+      (user-error "No matches found for %s" symbol))))
 
 (defun pyimport--extract-unused-var (flycheck-message)
   "Extract the import variable name from FLYCHECK-MESSAGE.
